60878527a9e682b290f3eac5e89465925f3c94bd
Raffaele Del Prete <raffaeledelprete@MBP-di-Raffaele.fritz.box>
2022-06-23 19:35:50 +0200

    change arg as props

--- pages/hello/[name].js
+++ pages/hello/[name].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 
-const Hello = ({ name }) => {
-    return <p>Hello {name}</p>
+const Hello = (props) => {
+    return <p>Hello {props.name}</p>
 }
 
 export default Hello
